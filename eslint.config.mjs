3532939cf96f0b86d3a1eea04243cf582eb65dcc
jenny <cjennifer2318@gmail.com>
2026-05-20 22:38:57 +0300

    build: add fastify and nestjs-spelunker dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,21 +18,30 @@ export const nestJsConfig = [
 	{
 		languageOptions: {
 			globals: {
-				...globals.serviceworker
-			}
-		}
+				...globals.serviceworker,
+			},
+		},
 	},
 	{
 		rules: {
 			'@typescript-eslint/interface-name-prefix': 'off',
 			'@typescript-eslint/explicit-function-return-type': 'off',
 			'@typescript-eslint/explicit-module-boundary-types': 'off',
-			'@typescript-eslint/no-explicit-any': 'off'
-		}
+			'@typescript-eslint/no-explicit-any': 'off',
+		},
 	},
 	{
 		...prettier_plugin,
 		name: 'prettier/recommended',
-		ignores: [...default_ignores_pool]
-	}
+		ignores: [...default_ignores_pool],
+	},
+	{
+		rules: {
+			'max-depth': ['error', 2], // limit the depth of if statements
+			'no-else-return': ['error'], // enforce return statement at the end of functions, making them clearer
+			'max-lines': ['error', { max: 800, skipComments: true }], // limit the size of the file (exceptions may apply)
+			'max-lines-per-function': ['error', { max: 60, skipComments: true }], // limit the size of functions
+		},
+		ignores: ['src/generated/i18n.generated.ts'],
+	},
 ];
